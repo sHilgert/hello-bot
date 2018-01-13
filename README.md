a78c511414987429d7d6a640d8938ce944ce7c89
Sérgio Hilgert Junior <hilgertfacul@gmail.com>
2018-01-13 19:18:49 -0200

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Hello chat bot
+
+Um modelo simples para um chat bot que retorna um Hello para o usuario, via telegram.s
---
+++ chat_bot.js
@@ -0,0 +1,32 @@
+const TelegramBot = require('node-telegram-bot-api');
+
+const TOKEN = "335662340:AAECBj8izyXCIkdJFGgRRixbNelTvr2w0lA";
+const PORT = process.env.PORT || 5000;
+const URL = "http://url-do-heroku-do-seu-projeto"
+
+// Bot
+const botOptions = {
+  webHook: {
+    port: PORT
+  }
+};
+const bot = new TelegramBot(TOKEN, botOptions);
+//bot.setWebHook(`${URL}/bot${TOKEN}`);
+
+//Error Handlers
+bot.on('polling_error', (error) => console.log(error.code));
+bot.on('webhook_error', (error) => console.log(error.code));
+
+bot.onText(/\/echo (.+)/, (msg, match) => {
+  const chatId = msg.chat.id;
+  const resp = match[1]; 
+  bot.sendMessage(chatId, resp);
+});
+
+
+bot.on('message', (msg) => {
+  const chatId = msg.chat.id;
+
+  // send a message to the chat acknowledging receipt of their message
+  bot.sendMessage(chatId, 'hello');
+});
